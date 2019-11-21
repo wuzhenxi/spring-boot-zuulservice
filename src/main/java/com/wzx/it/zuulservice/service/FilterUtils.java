dc0f7c7cb6a6f0a972705b4358875a08682ae072
wuzhenxi <wuzx@hustcad.com>
2019-11-21 11:01:26 +0800

    【type】:add 【author】:wzx 【description】:新增zuul代码

---
+++ src/main/java/com/wzx/it/zuulservice/service/FilterUtils.java
@@ -0,0 +1,29 @@
+package com.wzx.it.zuulservice.service;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterUtils extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
